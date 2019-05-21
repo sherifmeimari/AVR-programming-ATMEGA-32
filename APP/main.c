a5fa5f33df7533fe96ef9e2f03e0ed9983ea40c6
Sherif MEIMARI <sherifmeimari@gmail.com>
2019-05-21 15:10:33 +0200

    Update main.c

--- APP/main.c
+++ APP/main.c
@@ -8,6 +8,7 @@
 #include "avr/delay.h"
 #include "DIO_int.h"
 #include "SWITCH_int.h"
+#include "LED_int.h"
 #include "avr/io.h"
 
 
@@ -15,14 +16,31 @@ int main() {
 
 	DIO_voidInit();
 	SWITCH_voidInit();
+	LED_voidInit();
 
 	while(1) {
 
+		// Check first LED
 		if(SWITCH_ISPressed(SWITCH_0) == IS_PRESSED) {
-			DIO_voidSetPin(DIO_PIN_31, DIO_HIGH);
+			LED_ON(LED_0);
 		} else {
-			DIO_voidSetPin(DIO_PIN_31, DIO_LOW);
+			LED_OFF(LED_0);
 		}
+
+		// Check second LED
+		if(SWITCH_ISPressed(SWITCH_1) == IS_PRESSED) {
+			LED_ON(LED_1);
+		} else {
+			LED_OFF(LED_1);
+		}
+
+		// Check third LED
+		if(SWITCH_ISPressed(SWITCH_2) == IS_PRESSED) {
+			LED_ON(LED_2);
+		} else {
+			LED_OFF(LED_2);
+		}
+
 	}
 
 	return 0;
